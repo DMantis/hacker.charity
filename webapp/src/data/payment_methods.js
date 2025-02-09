199cb84c9fc818b351691745151198d2f825053c
Dmitry Mantis <DMantis@users.noreply.github.com>
2025-02-09 16:36:35 +0400

    add frida, fix homebank link and move payment methods to the dedicated file

---
+++ webapp/src/data/payment_methods.js
@@ -0,0 +1,42 @@
+export const paymentMethodsMap = {
+    // cards/bank section
+    "liberapay": { name: "liberapay", icon: "liberapay" },
+    "github": { name: "github sponsors", icon: "github" },
+    "opencollective": { name: "open collective", icon: "opencollective" },
+    "patreon": { name: "patreon", icon: "patreon" },
+    "kofi": { name: "ko-fi", icon: "kofi" },
+    "bank-transfer-eu": { name: "eu bank transfer", icon: "bank-transfer" },
+    "bank-transfer-us": { name: "us bank transfer", icon: "bank-transfer" },
+    "bank-transfer": { name: "bank transfer", icon: "bank-transfer" },  // todo: merge eu and others?
+    "buymeacoffee": { name: "buymeacoffee", icon: "buymeacoffee" },
+    "check-us": { name: "check (us)", icon: "" },
+    "check-ca": { name: "check (ca)", icon: "" },
+    "donorbox": { name: "donorbox", icon: "donorbox" },
+    "every.org": { name: "every.org", icon: "" },
+    "paypal": { name: "paypal", icon: "paypal" },
+    "card": { name: "debit/credit card", icon: "visa" },
+    "wise": { name: "wise", icon: "wise" },
+    "sepa": { name: "sepa", icon: "sepa" },
+    "stripe": { name: "stripe", icon: "stripe" },
+    "benevity": { name: "benevity", icon: "" },
+    // crypto section
+    "btc": { name: "bitcoin", icon: "bitcoin" },
+    "xmr": { name: "monero", icon: "monero" },
+    "eth": { name: "ethereum", icon: "ethereum" },
+    "ltc": { name: "litecoin", icon: "litecoin" },
+    "ada": { name: "cardano", icon: "cardano" },
+    "doge": { name: "doge", icon: "doge" },
+    "sol": { name: "solana", icon: "solana" },
+    "zec": { name: "zcash", icon: "zcash" },
+    "dot": { name: "polkadot", icon: "polkadot" },
+    "bch": { name: "bitcoin cash", icon: "bch" },
+    "btc-ln": { name: "bitcoin lightning", icon: "bitcoin" },
+    "btc-sp": { name: "bitcoin silent payments", icon: "bitcoin" },
+    "xlm": { name: "stellar", icon: "stellar" },
+    // stablecoin section
+    "usdt-erc20": { name: "usdt erc20", icon: "tether" },
+    "usdt-trc20": { name: "usdt trc20", icon: "tether" },
+    "usdc-erc20": { name: "usdc erc20", icon: "" },
+    "dai-erc20": { name: "dai erc20", icon: "" }
+};
+
--- webapp/src/data/projects.js
+++ webapp/src/data/projects.js
@@ -350,7 +350,7 @@ export const projectsData = [
         description: "terminal recording",
         donation_page: "https://docs.asciinema.org/donations/"
     },
-        {
+    {
         name: "OpenShot",
         link: "https://www.openshot.org/",
         tags: ["media", "videoediting", "linux", "macos", "windows"],
@@ -361,7 +361,7 @@ export const projectsData = [
     },
     {
         name: "HomeBank",
-        link: "http://homebank.free.fr/en/index.php",
+        link: "https://www.gethomebank.org/en/index.php",
         tags: ["finance", "accounting", "linux", "macos", "windows"],
         payment_methods: ["liberapay", "paypal"],
         methods_checked_by_hand: true,
@@ -1411,48 +1411,15 @@ export const projectsData = [
         methods_checked_by_hand: true,
         description: "GUI for V2Ray, VLESS, XRay VPN protocols.",
         donation_page: "https://github.com/MatsuriDayo/nekoray?tab=readme-ov-file#%E6%8D%90%E5%8A%A9--donate",
+    },
+    {
+        name: "frida",
+        link: "https://frida.re/",
+        tags: ["security"],
+        payment_methods: ["github"],
+        methods_checked_by_hand: true,
+        description: "Dynamic instrumentation toolkit for developers, reverse-engineers, and security researchers",
+        donation_page: "https://github.com/frida/frida"
     }
 ];
-
-export const paymentMethodsMap = {
-    // cards/bank section
-    "liberapay": { name: "liberapay", icon: "liberapay" },
-    "github": { name: "github sponsors", icon: "github" },
-    "opencollective": { name: "open collective", icon: "opencollective" },
-    "patreon": { name: "patreon", icon: "patreon" },
-    "kofi": { name: "ko-fi", icon: "kofi" },
-    "bank-transfer-eu": { name: "eu bank transfer", icon: "bank-transfer" },
-    "bank-transfer-us": { name: "us bank transfer", icon: "bank-transfer" },
-    "bank-transfer": { name: "bank transfer", icon: "bank-transfer" },  // todo: merge eu and others?
-    "buymeacoffee": { name: "buymeacoffee", icon: "buymeacoffee" },
-    "check-us": { name: "check (us)", icon: "" },
-    "check-ca": { name: "check (ca)", icon: "" },
-    "donorbox": { name: "donorbox", icon: "donorbox" },
-    "every.org": { name: "every.org", icon: "" },
-    "paypal": { name: "paypal", icon: "paypal" },
-    "card": { name: "debit/credit card", icon: "visa" },
-    "wise": { name: "wise", icon: "wise" },
-    "sepa": { name: "sepa", icon: "sepa" },
-    "stripe": { name: "stripe", icon: "stripe" },
-    "benevity": { name: "benevity", icon: "" },
-    // crypto section
-    "btc": { name: "bitcoin", icon: "bitcoin" },
-    "xmr": { name: "monero", icon: "monero" },
-    "eth": { name: "ethereum", icon: "ethereum" },
-    "ltc": { name: "litecoin", icon: "litecoin" },
-    "ada": { name: "cardano", icon: "cardano" },
-    "doge": { name: "doge", icon: "doge" },
-    "sol": { name: "solana", icon: "solana" },
-    "zec": { name: "zcash", icon: "zcash" },
-    "dot": { name: "polkadot", icon: "polkadot" },
-    "bch": { name: "bitcoin cash", icon: "bch" },
-    "btc-ln": { name: "bitcoin lightning", icon: "bitcoin" },
-    "btc-sp": { name: "bitcoin silent payments", icon: "bitcoin" },
-    "xlm": { name: "stellar", icon: "stellar" },
-    // stablecoin section
-    "usdt-erc20": { name: "usdt erc20", icon: "tether" },
-    "usdt-trc20": { name: "usdt trc20", icon: "tether" },
-    "usdc-erc20": { name: "usdc erc20", icon: "" },
-    "dai-erc20": { name: "dai erc20", icon: "" }
-};
 
